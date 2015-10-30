9ab250e3798db2f15bf3b4f31fa06f080f37663b
Zocoo <815769472@qq.com>
2015-10-30 17:28:44 +0800

    this is 明天的事情

--- test/src/hello.java
+++ test/src/hello.java
@@ -3,6 +3,6 @@ public class hello
 {
 	public void sayhello()
 	{
-		System.out.println("hello world! my heart is break!明天也许可以放假");
+		System.out.println("hello world! my heart is break!明天也许可以放假,我希望是可以的！");
 	}
 }
